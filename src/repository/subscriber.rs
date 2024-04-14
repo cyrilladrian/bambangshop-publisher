1c499ebc0ba566276ca0628ef895989974686d03
cyrilladrian <cyrilladrian.wicaksono@gmail.com>
2024-04-14 16:06:42 +0700

    Create Notification model struct

---
+++ src/repository/subscriber.rs
@@ -0,0 +1,8 @@
+use dashmap::DashMap;
+use lazy_static::lazy_static;
+use crate::model::subscriber::Subscriber;
+
+// Singleton of Database
+lazy_static! {
+    static ref SUBSCRIBERS: DashMap<usize, Subscriber> = DashMap::new();
+}
