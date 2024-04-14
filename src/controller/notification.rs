f0eff1007acdd76836fa67faacd1d2267ca5fe7b
cyrilladrian <cyrilladrian.wicaksono@gmail.com>
2024-04-14 17:51:38 +0700

    Implement unsubscribe function in Notification Controller

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -6,7 +6,6 @@ use crate::model::subscriber::Subscriber;
 use crate::service::notification::NotificationService;
 
 #[post("/subscribe/<product_type>", data = "<subscriber>")]
-
 pub fn subscribe(product_type: &str, subscriber: Json<Subscriber>) -> Result<Created<Json<Subscriber>>> {
     return match NotificationService::subscribe(product_type, subscriber.into_inner()) {
         Ok(f) => Ok(Created::new("/").body(Json::from(f))),
@@ -14,3 +13,10 @@ pub fn subscribe(product_type: &str, subscriber: Json<Subscriber>) -> Result<Cre
     };
 }
 
+#[post("/unsubscribe/<product_type>/<url>")]
+pub fn unsubscribe(product_type: &str, url:&str) -> Result<Json<Subscriber>> {
+    return match NotificationService::unsubscribe(product_type, url) {
+        Ok(f) => Ok(Json::from(f)),
+        Err(e) => Err(e),
+    };
+}
